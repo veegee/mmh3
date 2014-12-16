af30335e954e53a5df74d8a0ed90ffc831aac271
veegee <veegee@veegee.org>
2014-12-16 05:14:11 -0500

    Add test for hash128

--- tests/test_mmh3.py
+++ tests/test_mmh3.py
@@ -14,6 +14,10 @@ class Test64Bit:
         h1, h2 = mmh3.hash64('hello')
         assert (h1, h2) == (-3758069500696749310, 6565844092913065241)
 
+    def test_hash_128(self):
+        h = mmh3.hash128('hello')
+        assert h == 121118445609844952839898260755277781762
+
     def test_hash_bytes(self):
         h = mmh3.hash_bytes('foo')
         assert h == b'aE\xf5\x01W\x86q\xe2\x87}\xba+\xe4\x87\xaf~'
