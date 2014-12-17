5d2a3eba46704d5544f9209225d64db19648c8fb
veegee <veegee@veegee.org>
2014-12-17 00:28:03 -0500

    Remove 32-bit arch hashes

--- murmur_hash_3.h
+++ murmur_hash_3.h
@@ -9,5 +9,4 @@ typedef unsigned __int64 uint64_t;
 #endif
 
 void MurmurHash3_x86_32(const void *key, int len, uint32_t seed, void *out);
-void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
